df4444fee2d64aaa297bb31b8ab886921e114b6f
Evgenius2020 <spleen47@mail.ru>
2017-03-20 18:21:27 +0700

    (#7) 21\34

---
+++ 7.Calculator/Calculator/Stack.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include "Stack.h"
+
+Stack* stackCreate() {
+	Stack* stack = (Stack*)malloc(sizeof(Stack));
+	stack->top = NULL;
+}
+
+void stackDestroy(Stack* stack) {
+	while (!stackIsEmpty(stack)) {
+		stackPop(stack);
+	}
+
+	free(stack);
+}
+
+void stackPush(Stack* stack, int data) {
+	StackElement* element = (StackElement*)malloc(sizeof(StackElement));
+	if (!element) {
+		return;
+	}
+	element->data = data;
+
+	if (stackIsEmpty(stack)) {
+		stack->top = element;
+		element->next = NULL;
+	}
+	else {
+		element->next = stack->top;
+		stack->top = element;
+	}
+}
+
+int stackPop(Stack* stack) {
+	if (stackIsEmpty(stack)) {
+		return NULL;
+	}
+
+	StackElement* element = stack->top;
+	stack->top = element->next;
+	int data = element->data;
+	free(element);
+
+	return data;
+}
+
+int stackPeek(Stack* stack) {
+	if (stackIsEmpty(stack)) {
+		return NULL;
+	}
+
+	return stack->top->data;
+}
+
+char stackIsEmpty(Stack* stack) {
+	if (stack->top == NULL) {
+		return 1;
+	}
+
+	return 0;
+}
---
+++ 7.Calculator/Calculator/main.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include "Stack.h"
+
+#define exit fclose(in); fclose(out); return 0
+
+char getPrior(char chr) {
+	if ((chr == '+') || (chr == '-')) {
+		return 2;
+	}
+	if ((chr == '*') || (chr == '/')) {
+		return 3;
+	}
+	if ((chr == '(') || (chr == ')')) {
+		return 1;
+	}
+	if ((-1 == chr) || (chr == '\n')) {
+		return 0;
+	}
+
+	return -1;
+}
+
+int main() {
+	FILE* in = fopen("in.txt", "r");
+	FILE* out = fopen("out.txt", "w");
+
+	Stack* numbers = stackCreate();
+	Stack* signs = stackCreate();
+
+	char curr;
+	int numCollector = -1;
+	int left, right;
+	char brasketFlag = 0;
+	while (!feof(in)) {
+		curr = fgetc(in);
+		if ((curr >= '0') && (curr <= '9')) {
+			if (numCollector == -1) {
+				numCollector = curr - '0';
+			}
+			else {
+				numCollector = numCollector * 10 + curr - '0';
+			}
+			continue;
+		}
+		if (getPrior(curr) == -1) {
+			fprintf(out, "syntax error\n");
+			exit;
+		}
+		if (numCollector != -1) {
+			stackPush(numbers, numCollector);
+			numCollector = -1;
+		}
+		if (curr == ')') {
+			brasketFlag = 1;
+		}
+		if (curr == '(') {
+			stackPush(signs, '(');
+			continue;
+		}
+		while ((brasketFlag) ||
+			(!stackIsEmpty(signs) && (getPrior(curr) <= getPrior(stackPeek(signs))))) {
+			if (stackPeek(signs) == '(') {
+				if (brasketFlag == 1) { /* 3+2() */
+					fprintf(out, "syntax error\n");
+					exit;
+				}
+				brasketFlag = 0;
+				stackPop(signs);
+				break;
+			}
+			if (stackIsEmpty(numbers)) {
+				fprintf(out, "syntax error\n");
+				exit;
+			}
+			right = stackPop(numbers);
+			if (stackIsEmpty(numbers)) {
+				fprintf(out, "syntax error\n");
+				exit;
+			}
+			left = stackPop(numbers);
+			switch (stackPeek(signs)) {
+			case '-': {
+				stackPush(numbers, left - right);
+				break;
+			}
+			case '+': {
+				stackPush(numbers, left + right);
+				break;
+			}
+			case '*': {
+				stackPush(numbers, left * right);
+				break;
+			}
+			case '/': {
+				if (right == 0) {
+					fprintf(out, "division by zero\n");
+					exit;
+				}
+				stackPush(numbers, left / right);
+				break;
+			}
+			}
+			if (brasketFlag) {
+				brasketFlag = 2;
+			}
+			stackPop(signs);
+		}
+		if ((curr != ')') && (curr != EOF) && (curr != '\n')) {
+			stackPush(signs, curr);
+		}
+	}
+	if (!stackIsEmpty(signs)) {
+		fprintf(out, "syntax error\n");
+	}
+	else {
+		fprintf(out, "%d", stackPop(numbers));
+	}
+	exit;
+}
