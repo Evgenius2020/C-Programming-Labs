28350dbfe7ad2f7c4f5a1afb144b6d4976305ae4
Evgenius2020 <spleen47@mail.ru>
2017-03-28 13:28:23 +0700

    (#7) Include path normalized

--- 07. Calculator/Source/main.c
+++ 07. Calculator/Source/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../../Common/Stacks/Stack-int/Source/Stack.c"
+#include "Stack.h"
 
 #define MAX_STRLEN 1024
 #define isNumber(val) (('0' <= val) && (val <= '9'))
