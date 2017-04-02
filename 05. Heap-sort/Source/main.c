dbfcbb7cf9efe1b2c4c75acec32220eb52974eba
Evgenius2020 <spleen47@mail.ru>
2017-04-02 12:41:13 +0700

    (5) Works

--- 05. Heap-sort/Source/main.c
+++ 05. Heap-sort/Source/main.c
@@ -1,26 +1,80 @@
 #include <stdio.h>
-#include "Heap.h"
+#include <stdlib.h>
 
-int main() {
+typedef int key;
+
+void heapHeapify(key* mass, int length, int i) {
+	int left = 2 * i;
+	int right = 2 * i + 1;
+	key largest = i;
+	if ((left <= length) && (mass[left - 1] > mass[largest - 1])) {
+		largest = left;
+	}
+	if ((right <= length) && (mass[right - 1] > mass[largest - 1])) {
+		largest = right;
+	}
+	if (largest != i) {
+		mass[i - 1] += mass[largest - 1];
+		mass[largest - 1] = mass[i - 1] - mass[largest - 1];
+		mass[i - 1] -= mass[largest - 1];
+		heapHeapify(mass, length, largest);
+	}
+}
+
+void heapBuild(key* mass, int length) {
+	int i;
+	for (i = length / 2; i > 0; i--) {
+		heapHeapify(mass, length, i);
+	}
+}
+
+void heapChangeKey(key* mass, int i, key val) {
+	if (val < mass[i]) {
+		return;
+	}
+	mass[i] = val;
+	while ((i > 0) && (mass[i/2] < mass[i])) {
+		mass[i / 2] += mass[i];
+		mass[i] = mass[i / 2] - mass[i];
+		mass[i / 2] -= mass[i];
+		i /= 2;
+	}
+}
+
+void heapsort(key* mass, int length) {
+	heapBuild(mass, length);
+	int i;
+	for (i = length; i > 1; i--) {
+		mass[0] += mass[i - 1];
+		mass[i - 1] = mass[0] - mass[i - 1];
+		mass[0] -= mass[i - 1];
+		length--;
+		heapHeapify(mass, length, 1);
+	}
+}
+
+void main() {
 	FILE* in = fopen("in.txt", "r");
 	FILE* out = fopen("out.txt", "w");
+	if ((!in) || (!out)) {
+		return;
+	}
 
-	int length;
+	int length, i;
+	key* mass;
+
 	fscanf(in, "%d ", &length);
-	Heap* heap = heapCreate(length);
-	int newNumber;
-	while (length) {
-		fscanf(in, "%d ", &newNumber);
-		heapInsert(heap, newNumber);
-		length--;
+	mass = malloc(sizeof(key)* length);
+	for (i = 0; i < length; i++) {
+		fscanf(in, "%d ", &mass[i]);
 	}
 
-	while (heap->length) {
-		fprintf(out, "%d ", heapExtractMin(heap));
+	heapsort(mass, length);
+
+	for (i = 0; i < length; i++) {
+		fprintf(out, "%d ", mass[i]);
 	}
 
-	heapDestroy(heap);
 	fclose(in);
 	fclose(out);
-	return 0;
 }
