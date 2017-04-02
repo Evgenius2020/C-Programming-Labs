e3669bcf638be227b7b005302e0adca340acfe3a
Evgenius2020 <spleen47@mail.ru>
2017-04-02 12:53:03 +0700

    (5) Separated

--- 05. Heap-sort/Source/main.c
+++ 05. Heap-sort/Source/main.c
@@ -1,45 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef int key;
-
-void heapHeapify(key* mass, int length, int i) {
-	int left = 2 * i;
-	int right = 2 * i + 1;
-	key largest = i;
-	if ((left <= length) && (mass[left - 1] > mass[largest - 1])) {
-		largest = left;
-	}
-	if ((right <= length) && (mass[right - 1] > mass[largest - 1])) {
-		largest = right;
-	}
-	if (largest != i) {
-		mass[i - 1] += mass[largest - 1];
-		mass[largest - 1] = mass[i - 1] - mass[largest - 1];
-		mass[i - 1] -= mass[largest - 1];
-		heapHeapify(mass, length, largest);
-	}
-}
-
-void heapBuild(key* mass, int length) {
-	int i;
-	for (i = length / 2; i > 0; i--) {
-		heapHeapify(mass, length, i);
-	}
-}
-
-void heapChangeKey(key* mass, int i, key val) {
-	if (val < mass[i]) {
-		return;
-	}
-	mass[i] = val;
-	while ((i > 0) && (mass[i/2] < mass[i])) {
-		mass[i / 2] += mass[i];
-		mass[i] = mass[i / 2] - mass[i];
-		mass[i / 2] -= mass[i];
-		i /= 2;
-	}
-}
+#include "MaxHeap.h"
 
 void heapsort(key* mass, int length) {
 	heapBuild(mass, length);
