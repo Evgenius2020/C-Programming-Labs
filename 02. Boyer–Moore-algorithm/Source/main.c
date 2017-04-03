d625db6b8710273d359a4e6156dfffaf082d560e
Evgenius2020 <spleen47@mail.ru>
2017-04-03 20:09:46 +0700

    (2) Ring buffer

--- 02. Boyer–Moore-algorithm/Source/main.c
+++ 02. Boyer–Moore-algorithm/Source/main.c
@@ -2,23 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* readTemplate(FILE* in) {
-	char* result = (char*)malloc(sizeof(char) * 18);
+typedef struct RingBufferElement {
+	struct RingBufferElement* prev;
+	struct RingBufferElement* next;
+	unsigned char chr;
+} RingBufferElement;
+
+RingBufferElement* ringBufferBuild(short length) {
+	if (!length) {
+		return NULL;
+	}
+
+	RingBufferElement* begin = (RingBufferElement*)malloc(sizeof(RingBufferElement));
+	RingBufferElement* prev = begin;
+	RingBufferElement* curr = begin;
+	for (length--; length > 0; length--) {
+		curr = (RingBufferElement*)malloc(sizeof(RingBufferElement));
+		curr->prev = prev;
+		prev->next = curr;
+		prev = curr;
+	}
+	curr->next = begin;
+	begin->prev = curr;
+
+	return begin;
+}
+
+void RingBufferDestroy(RingBufferElement* element) {
+	/*while (element->next) {
+		free(element->prev);
+		element->prev = NULL;
+		element = element->next;
+	}
+	free(element);*/
+}
+
+RingBufferElement* ringBufferRead(FILE* in, RingBufferElement* curr, short bytes) {
+	while (bytes) {
+		curr = curr->next;
+		curr->chr = fgetc(in);
+		if (curr->chr == EOF) {
+			break;
+		}
+		bytes--;
+	}
+	return curr;
+}
+
+unsigned char* readTemplate(FILE* in) {
+	unsigned char* result = (unsigned char*)malloc(sizeof(char) * 18);
 	if (fgets(result, 18, in) == NULL) {
 		return;
 	}
 	if (result[strlen(result) - 1] == '\n') {
 		result[strlen(result) - 1] = '\0';
-	}
-	return result;
-}
-
-char* readText(FILE* in) {
-	char* result = (char*)malloc(sizeof(char) * 256);
-	result[0] = '\0';
-	char newString[256];
-	while (fgets(newString, 256, in)) {
-		strcat(result, newString);
 	}
 	return result;
 }
@@ -31,12 +68,14 @@ int main() {
 	}
 
 	char shifts[256];
-	short i, j;
-	char* templt = readTemplate(in);
-	char* text = readText(in);
-	if (!strlen(text)) {
-		return;
-	}
+	short i;
+	unsigned long long textPos;
+	unsigned char* templt = readTemplate(in);
+	RingBufferElement* curr = ringBufferBuild(strlen(templt));
+	RingBufferElement* buf;
+	ringBufferRead(in, curr, strlen(templt));
+	textPos = strlen(templt);
+
 	for (i = 0; i < 256; i++) {
 		shifts[i] = strlen(templt);
 	}
@@ -44,18 +83,25 @@ int main() {
 		shifts[(unsigned char)templt[i]] = strlen(templt) - i - 1;
 	}
 
-	i = strlen(templt) - 1;
-	while (i < strlen(text)) {
-		for (j = 0; j < strlen(templt); j++) {
-			fprintf(out, "%d ", i - j + 1);
-			if (templt[strlen(templt) - 1 - j] != text[i - j]) {
-				break;
+	i = strlen(templt);
+	buf = curr;
+	while (curr->chr != 255) {
+		if (i > 0) {
+			fprintf(out, "%d ", textPos - (strlen(templt) - i));
+			if (buf->chr == templt[i - 1]) {
+				i--;
+				buf = buf->prev;
+				continue;
 			}
 		}
-		i += shifts[(unsigned char)text[i]];
+
+		textPos += shifts[curr->chr];
+		curr = ringBufferRead(in, curr, shifts[curr->chr]);
+		buf = curr;
+		i = strlen(templt);
 	}
 
+	RingBufferDestroy(curr);
 	fclose(in);
 	fclose(out); 
-	return 0;
 }
