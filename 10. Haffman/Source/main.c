7bb16649c08dd384e5d020fe6e045f3fb37385b8
Evgenius2020 <spleen47@mail.ru>
2017-04-05 22:43:29 +0700

    (10) Input files capturing

--- 10. Haffman/Source/main.c
+++ 10. Haffman/Source/main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "Encoding.h"
 #include "Decoding.h"
+
+char* makeFileName(char testName) {
+	char* filename = (char*)calloc(5 + 5 + 1, sizeof(char));
+	strcat(filename, "test/");
+	filename[5] = testName;
+	strcat(filename, ".txt");
+	return filename;
+}
 
 int main() {
 	FILE* in = fopen("in.txt", "r");
 	FILE* out = fopen("out.txt", "w");
-	
+
+	char currTest = 'a';
+	FILE* copy;
+	while (1){
+		copy = fopen(makeFileName(currTest), "r");
+		if (!copy) {
+			break;
+		}
+		currTest++;
+	}
+	copy = fopen(makeFileName(currTest), "w");
+	short curr;
+	while(1) {
+		curr = fgetc(in);
+		if (curr == -1) {
+			break;
+		}
+		fputc(curr, copy);
+	}
+	fclose(copy);
+	rewind(in);
+
 	char buf[5];
 	fgets(&buf, 5, in);
 	char cmd = buf[0];
