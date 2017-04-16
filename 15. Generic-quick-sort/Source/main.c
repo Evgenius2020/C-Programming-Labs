a43b2110fe39481bfb392cb5b3d881942eaa6028
Evgenius2020 <spleen47@mail.ru>
2017-04-16 19:11:32 +0700

    (15) Workable with strcmp

--- 15. Generic-quick-sort/Source/main.c
+++ 15. Generic-quick-sort/Source/main.c
@@ -10,19 +10,22 @@ typedef struct Row {
 } Row;
 
 char nameCmp(Row* left, Row* right) {
-	return strcmp(left, right);
+	return strcmp(left->name, right->name);
 }
 
+static void* buf;
+static void* pivot;
+
+#define NUMCMP(left, right, fieldname) return (left->##fieldname == right->##fieldname) ? 0 : ((left->##fieldname < right->##fieldname) ? -1 : 1)
+
 char ageCmp(Row* left, Row* right) {
-	if (left->age != left->age) {
-		if (left->age < left->age) {
-			return -1;
-		}
-		else {
-			return 1;
-		}
-	}
-	return 0;
+	NUMCMP(left, right, age);
+}
+char markCmp(Row* left, Row* right) {
+	NUMCMP(left, right, mark);
+}
+char groupCmp(Row* left, Row* right) {
+	NUMCMP(left, right, group);
 }
 
 void qSortRec(void* mass, size_t elemSize, int l, int r, char *cmp(void*, void*)) {
@@ -30,21 +33,20 @@ void qSortRec(void* mass, size_t elemSize, int l, int r, char *cmp(void*, void*)
 		return;
 	}
 
-	void *pivot, *buf;
 	int i = l;
 	int j = r;
-	pivot = &mass + ((l + r) / 2) * elemSize;
+	memcpy(pivot, (char*)mass + ((l + r) / 2) * elemSize , elemSize);
 	do {
-		while (cmp(&mass + i*elemSize, pivot) < 0) {
+		while (strcmp((char*)mass + i*elemSize, pivot) < 0) {
 			i++;
 		}
-		while (cmp(pivot, &mass + j * elemSize) > 0) {
+		while (strcmp(pivot, (char*)mass + j*elemSize) < 0) {
 			j--;
 		}
 		if (i <= j) {
-			buf = &mass + i*elemSize;
-			*(&mass + i*elemSize) = &mass + j*elemSize;
-			*(&mass + j*elemSize) = buf;
+			memcpy(buf, (char*)mass + i*elemSize, elemSize);
+			memcpy(((char*)mass + i*elemSize), ((char*)mass + j*elemSize), elemSize);
+			memcpy(((char*)mass + j*elemSize), buf, elemSize);
 			i++;
 			j--;
 		}
@@ -55,7 +57,11 @@ void qSortRec(void* mass, size_t elemSize, int l, int r, char *cmp(void*, void*)
 }
 
 void qSort(void* mass, int length, size_t elemSize, char *cmp(void*, void*)) {
+	buf = malloc(elemSize);
+	pivot = malloc(elemSize);
 	qSortRec(mass, elemSize, 0, length - 1, cmp);
+	free(pivot);
+	free(buf);
 }
 
 void main(int argc, char* argv[]) {
@@ -80,7 +86,16 @@ void main(int argc, char* argv[]) {
 	}
 
 	if (!strcmp(argv[3], "-name")) {
-		qsort(rows, rowsN, sizeof(Row), nameCmp);
+		qSort(rows, rowsN, sizeof(Row), nameCmp);
+	}
+	if (!strcmp(argv[3], "-age")) {
+		qSort(rows, rowsN, sizeof(Row), ageCmp);
+	}
+	if (!strcmp(argv[3], "-mark")) {
+		qSort(rows, rowsN, sizeof(Row), markCmp);
+	}
+	if (!strcmp(argv[3], "-group")) {
+		qSort(rows, rowsN, sizeof(Row), groupCmp);
 	}
 
 	for (i = 0; i < rowsN; i++) {
