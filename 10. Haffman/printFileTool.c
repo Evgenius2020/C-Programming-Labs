99540677fec66617e3f11a2cf4e63694855dc697
Evgenius2020 <spleen47@mail.ru>
2017-04-30 01:10:57 +0700

    (10) Refactoring

--- 10. Haffman/printFileTool.c
+++ 10. Haffman/printFileTool.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
+/* Prints file in ASCII-codes */
 void main(int argc, char* argv[]) {
-	FILE*  in = fopen(argv[1], "r");
+	FILE*  in = fopen(argv[1], "rb");
 	int curr;
 	while(1) {
 		curr = fgetc(in);
